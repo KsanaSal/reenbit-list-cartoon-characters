186d99d1868020464a42198e9bd8faf72534bb5b
KsanaSal <oasalivon@gmail.com>
2023-03-19 22:38:51 +0200

    Added styles character details page

--- src/pages/CharacterDetails.tsx
+++ src/pages/CharacterDetails.tsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import getCharacterId from "../data/getCharacterId";
 import { Character } from "../interfaces/character.interface";
+import arrowBackIcon from "../assets/icons/arrow_back-icon.svg";
+import css from "./CharacterDetails.module.css";
 
 const CharacterDetails = () => {
     const [character, setCharacter] = useState<Character | null>(null);
@@ -25,31 +27,41 @@ const CharacterDetails = () => {
 
     return (
         <div>
+            <a href="/reenbit-list-cartoon-characters" className={css.link}>
+                <img src={arrowBackIcon} alt="Arrow back" />
+                GO BACK
+            </a>
             {character && (
                 <>
-                    <img src={character.image} alt="Hero" />
-                    <h1>{character.name}</h1>
-                    <h2>Information</h2>
-                    <ul>
-                        <li>
-                            <h3>Gender</h3>
-                            <p>{character.gender}</p>
+                    <div className={css.wrapImage}>
+                        <img src={character.image} alt="Hero" />
+                    </div>
+                    <h1 className={css.title}>{character.name}</h1>
+                    <h2 className={css.titleList}>Information</h2>
+                    <ul className={css.list}>
+                        <li className={css.listItem}>
+                            <h3 className={css.itemTitle}>Gender</h3>
+                            <p className={css.itemText}>{character.gender}</p>
                         </li>
-                        <li>
-                            <h3>Status</h3>
-                            <p>{character.status}</p>
+                        <li className={css.listItem}>
+                            <h3 className={css.itemTitle}>Status</h3>
+                            <p className={css.itemText}>{character.status}</p>
                         </li>
-                        <li>
-                            <h3>Specie</h3>
-                            <p>{character.species}</p>
+                        <li className={css.listItem}>
+                            <h3 className={css.itemTitle}>Specie</h3>
+                            <p className={css.itemText}>{character.species}</p>
                         </li>
-                        <li>
-                            <h3>Origin</h3>
-                            <p>{character.origin.name}</p>
+                        <li className={css.listItem}>
+                            <h3 className={css.itemTitle}>Origin</h3>
+                            <p className={css.itemText}>
+                                {character.origin.name}
+                            </p>
                         </li>
-                        <li>
-                            <h3>Type</h3>
-                            <p>{character.type || "Unknown"}</p>
+                        <li className={css.listItem}>
+                            <h3 className={css.itemTitle}>Type</h3>
+                            <p className={css.itemText}>
+                                {character.type || "Unknown"}
+                            </p>
                         </li>
                     </ul>
                 </>
